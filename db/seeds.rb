5fefba06127d7bd8db11e7e6e371a84c6f08de2d
kai5108 <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2021-04-04 10:37:21 +0000

    user validate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+   email: 'test@test.com',
+   password: 'testtest'
+)
+
+# User.create!(
+#    email: 'a@a.com',
+#    last_name: 'a',
+#    first_name: 'a',
+#    k_last_name: 'a',
+#    k_first_name: 'a',
+#    nickname: 'a',
+#    postal_code: '0000000',
+#    address: '大阪府大阪市中央区1-1-1',
+#    telephone_number: '00000000000',
+# )
