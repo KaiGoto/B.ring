ef8545ae7ce7c8ca67458f1c09e7cf4392c88325
kai5108 <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2021-04-06 04:30:17 +0000

    save

--- app/controllers/admins/regions_controller.rb
+++ app/controllers/admins/regions_controller.rb
@@ -1,9 +1,12 @@
 class Admins::RegionsController < ApplicationController
 before_action :authenticate_admin!
   def index
+    @regions = Region.all
+    @region = Region.new
   end
 
   def show
+    @regions = Region
   end
 
   def edit
--- app/controllers/public/regions_controller.rb
+++ app/controllers/public/regions_controller.rb
@@ -1,4 +1,15 @@
 class Public::RegionsController < ApplicationController
   def index
+    @region = Region.all
   end
+
+  def show
+    @regions = Region.find(params[:id])
+  end
+
+  private
+  def region_params
+    params.require(:region).permit(:name)
+  end
+
 end
