1ddf4589ab081f9cc3bf5932b4a457394a727d87
kai5108 <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2021-04-06 11:33:53 +0000

    regionの動作確認

--- app/controllers/admins/places_controller.rb
+++ app/controllers/admins/places_controller.rb
@@ -1,22 +1,47 @@
 class Admins::PlacesController < ApplicationController
 before_action :authenticate_admin!
   def index
+    @place = Place.page(params[:page]).per(10)
+    @region = Region.all
+    if params[:name].present?
+      @region = @region.get_by_name params[:name]
+    end
   end
 
   def show
+    @place = Place.find(params[:id])
   end
 
   def edit
+    @place = Place.find(params[:id])
   end
 
   def new
+    @place = Place.new
+    @places = Place.all
   end
 
   def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to admins_place_path(@place)
+    else
+      render "new"
+    end
   end
 
   def update
+    @place = Place.find(params[:id])
+    if @place.update(place_params)
+      redirect_to admins_place_path
+    else
+      render "edit"
+    end
   end
 
+  private
+  def place_params
+    params.require(:place).permit(:image_id, :name, :explanation, :region, :ground)
+  end
 
 end
--- app/controllers/admins/regions_controller.rb
+++ app/controllers/admins/regions_controller.rb
@@ -1,20 +1,40 @@
 class Admins::RegionsController < ApplicationController
 before_action :authenticate_admin!
   def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def new
+    @region = Region.all
+    @regions = Region.new
+    @region_all = Region.all
   end
 
   def create
+    @region = Region.new(region_params)
+    if @region.save
+      redirect_to admins_region_path(@region)
+    else
+      render "index"
+    end
+  end
+
+  def show
+    @region = Region.find(params[:id])
+    @place = Place.new
+  end
+
+  def edit
+    @region = Region.find(params[:id])
   end
 
   def update
+    @region = Region.find(params[:id])
+    if @region.update(region_params)
+      redirect_to admins_regions_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def region_params
+    params.require(:region).permit(:name)
   end
 end
