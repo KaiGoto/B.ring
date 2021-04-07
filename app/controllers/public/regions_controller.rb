5ac913d62d27ead21ed03d20aa45f2312bbb0e51
kai5108 <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2021-04-07 06:46:08 +0000

    region ok

--- app/controllers/public/regions_controller.rb
+++ app/controllers/public/regions_controller.rb
@@ -1,17 +1,13 @@
 class Public::RegionsController < ApplicationController
   def index
     @region = Region.all
+    @regions = Region.new
     @region_all = Region.all
   end
 
   def show
     @regions = Region.find(params[:id])
      @place = Place.all
-  end
-
-  private
-  def region_params
-    params.require(:region).permit(:name)
   end
 
 end
