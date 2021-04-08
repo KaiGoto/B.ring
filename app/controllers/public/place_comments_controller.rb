162e08a1b7f01d5461f1aa68f5cfc8cb364ae8a2
kai5108 <ec2-user@ip-172-31-12-54.ap-northeast-1.compute.internal>
2021-04-08 12:13:17 +0000

    いいね表示場所変更

--- app/controllers/public/place_comments_controller.rb
+++ app/controllers/public/place_comments_controller.rb
@@ -1,11 +1,11 @@
 class Public::PlaceCommentsController < ApplicationController
 
   def create
-    place = Place.find(params[:place_id])
+    @place = Place.find(params[:place_id])
     comment = current_user.place_comments.new(place_comment_params)
-    comment.place_id = place.id
+    comment.place_id = @place.id
     comment.save
-    redirect_to place_path(place)
+    redirect_to place_path(@place)
   end
   def destroy
     PlaceComment.find_by(id: params[:id], place_id: params[:place_id]).destroy
