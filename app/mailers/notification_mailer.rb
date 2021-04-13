49989174717c3d4a9b8c96cc969770e87476ad0e
kai5108 <k.510.ex.24@gmail.com>
2021-04-13 12:22:27 +0000

    complete

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationMailer < ApplicationMailer
   def contact_created(contact)
     @contact = contact
-    mail(to: ENV["TOMAIL"], subject: 'contact created')
+    mail(to: contact.email, subject: 'contact created')
   end
 end
