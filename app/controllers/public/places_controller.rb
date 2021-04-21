a3eca06510cb3137886efbc3a0343e7a58ca5726
kai5108 <k.510.ex.24@gmail.com>
2021-04-21 14:27:32 +0000

    complete

--- app/controllers/public/places_controller.rb
+++ app/controllers/public/places_controller.rb
@@ -1,14 +1,14 @@
 class Public::PlacesController < ApplicationController
   def index
-    @place = Place.page(params[:page]).per(5) #kaminari
+    # ランキング機能
+    @places = Place.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    @place = Kaminari.paginate_array(@places).page(params[:page]).per(5)
+    # @place = Place.page(params[:page]).per(5) #kaminari
     @place_all = Place.all
     @region = Region.all #地域名表示
     if params[:name].present?
     @region = @region.get_by_name params[:name]
     end
-    # ランキング機能
-    @places = Place.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
-    @place_kaminari = Kaminari.paginate_array(@places).page(params[:page]).per(5)
   end
 
   def show
