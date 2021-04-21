3fa9189f03a1c517c08bef87676586d12ecbfa39
kai5108 <k.510.ex.24@gmail.com>
2021-04-21 07:54:32 +0000

    save

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
    validates :name, presence: true, length:{minimum: 2,maximum: 30} #場所名
+   validates :email, presence: true, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10桁または11桁で入力してください' }  #メールアドレス
    validates :message, presence: true, length:{minimum: 2,maximum: 300} #メッセージ
 end
