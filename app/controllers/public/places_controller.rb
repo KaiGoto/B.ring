fb9b5ee0cd92e87027251d1293446bc3bfe2db55
kai5108 <k.510.ex.24@gmail.com>
2021-04-28 13:36:23 +0000

    no link

--- app/controllers/public/places_controller.rb
+++ app/controllers/public/places_controller.rb
@@ -5,10 +5,10 @@ class Public::PlacesController < ApplicationController
     @places = Place.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @place = Kaminari.paginate_array(@places).page(params[:page]).per(5)
     @place_all = Place.all
-    @region = Region.all 
+    @region = Region.all
     #地域名表示
     if params[:name].present?
-    @region = @region.get_by_name params[:name]
+    @regions = @regions.get_by_name params[:name]
     end
   end
 
